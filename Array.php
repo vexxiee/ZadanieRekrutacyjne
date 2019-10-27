3687397aeed82b03575a834b96dcfbd26006a62a
Łukasz Adamowicz <luk.adamowicz@gmail.com>
2019-10-27 13:32:32 +0100

    Update Array.php

--- Array.php
+++ Array.php
@@ -2,47 +2,24 @@
         class Matrix {
             private $row;
             private $col;
-                function __construct($row , $col){
+            private $name;
+                public function __construct(int $row , int $col, string $name){
                     $this->row=$row;
                     $this->col=$col;
+                    $this->name=$name;
                 }
-            function CreateMatrix(){
+            public function CreateMatrix(){
                 $row = $this->row;
                 $col = $this->col;
+                $name = $this->name;
                 print "<table class='mainTable'>";
-                print "<caption>Matrix A</caption>";
+                print "<caption>Matrix ".$name."</caption>";
                 for ($i=1; $i<=$row; $i++) {
                     print "<tr>";
                     for ($j=1; $j<=$col; $j++) {
-                        print "<td>".($matrix [$i][$j] = mt_rand(0,10))."</td>";
-                    }   
-                    print "</tr>"; 
-                }
-              print "</table>";
-              return ($matrix);
-    }
-}
-class Matrix2{
-    private $row;
-    private $col;
-        function __construct($row , $col){
-            $this->row=$row;
-            $this->col=$col;
-        }
-    function CreateMatrix(){
-        $row = $this->row;
-        $col = $this->col;
-        print "<table class='mainTable1'>";
-        print "<caption>Matrix B</caption>";
-        for ($x=1; $x<=$row; $x++) {
-            print "<tr>";
-            for ($y=1; $y<=$col; $y++) {
-                print "<td>".($matrix2 [$x][$y] = mt_rand(0,10))."</td>";
-            }  
-            print "</tr>";       
-        }
-        print "</table>";    
-      return ($matrix2);
-    }
+                        print "<td>".($array [$i][$j] = mt_rand(0,10))."</td>";
+                    }print "</tr>"; 
+                }print "</table>";
+              return ($array);}
 }
 ?>
