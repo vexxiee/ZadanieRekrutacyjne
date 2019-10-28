8047c5ec2135efb7b1a7e2527bc61d0f5b150fbb
Łukasz Adamowicz <luk.adamowicz@gmail.com>
2019-10-28 16:30:56 +0100

    Update Array.php

--- Array.php
+++ Array.php
@@ -1,25 +1,31 @@
 <?php
         class Matrix {
-            private $row;
-            private $col;
-            private $name;
-                public function __construct(int $row , int $col, string $name){
+            public $row , $col , $array;
+                public function __construct(int $row , int $col){
                     $this->row=$row;
                     $this->col=$col;
-                    $this->name=$name;
+                    for ($i=1; $i<=$row; $i++) {
+                        for ($j=1; $j<=$col; $j++) {
+                            ($array [$i][$j] = mt_rand(0,10));
+                        }
+                    }
+                  $this->array=$array;
                 }
-            public function CreateMatrix(){
-                $row = $this->row;
-                $col = $this->col;
-                $name = $this->name;
-                print "<table class='mainTable'>";
-                print "<caption>Matrix ".$name."</caption>";
-                for ($i=1; $i<=$row; $i++) {
-                    print "<tr>";
-                    for ($j=1; $j<=$col; $j++) {
-                        print "<td>".($array [$i][$j] = mt_rand(0,10))."</td>";
-                    }print "</tr>"; 
-                }print "</table>";
-        return ($array);}
-}
+                public function GetArray(){
+                    $array = $this->array;
+                    return $array;
+                }
+                public function GetNumRows() : int
+                {       
+                    
+                    $array = $this->GetArray();
+                    $Rows = count($array);
+                    return $Rows;
+                }
+                public function GetNumCols() : int
+                {
+                    $Cols = count($this->array[1]);
+                    return $Cols;
+                 }
+        }        
 ?>
