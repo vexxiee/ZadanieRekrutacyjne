858a9801d6b7195ed966b6aaaf70b5f15d34d104
Łukasz Adamowicz <luk.adamowicz@gmail.com>
2019-10-28 16:31:21 +0100

    Update Maths.php

--- Maths.php
+++ Maths.php
@@ -1,122 +1,74 @@
 <?php
-abstract class Maths{
+class Maths{
 
-    public $matrixArrayA;
-    public $matrixArrayB;
-
-        public function __construct($matrixArrayA, $matrixArrayB) 
-        {
-            $this->matrixArrayA = $matrixArrayA;
-            $this->matrixArrayB = $matrixArrayB;
-        }
-
-        public function GetRowNumA() : int
-        {
-            $rowA = count($this->matrixArrayA);
-            return $rowA;
-        }
-        public function GetRowNumB() : int
-        {
-            $rowB = count($this->matrixArrayB);
-            return $rowB;
-        }
-
-        public function GetColNumA() : int
-        {
-            $colA = count($this->matrixArrayA[1]);
-            return $colA;
-        }
-        public function GetColNumB() : int
-        {
-            $colB = count($this->matrixArrayB[1]);
-            return $colB;
-        }
-        
-}
-
-class Actions extends Maths{
+            public $matrixArrayA;
+            public $matrixArrayB;
+            public $row1, $row2, $col1, $col2;
+            public function __construct($matrixArrayA, $matrixArrayB, int $row1, int $row2, int $col1, int $col2) 
+            {
+                $this->matrixArrayA = $matrixArrayA;
+                $this->matrixArrayB = $matrixArrayB;
+                $this->row1 = $row1;
+                $this->row2 = $row2;
+                $this->col1 = $col1;
+                $this->col2 = $col2;
+            }
 
         public function AddMatrix(){
-
-            $rowA = $this->GetRowNumA();
-            $rowB = $this->GetRowNumB();
-            $colA = $this->GetColNumA();
-            $colB = $this->GetColNumB();
+            $rowA = $this->row1;
+            $rowB = $this->row2;
+            $colA = $this->col1;
+            $colB = $this->col2;
             $matrixArrayA = $this->matrixArrayA;
             $matrixArrayB = $this->matrixArrayB;
-            if ($rowA==$rowB && $colA==$colB){       // Sprawdzenie czy macierze sa tego samego rozmiaru
-                print "<table class='resultTable'>";
-                print "<caption>Aadding result</caption>";
-                 for ($k=1; $k<=$rowA; $k++){
-                 print "<tr>";
-                    for ($l=1; $l<=$colA; $l++){
-                        $adding = $matrixArrayA[$k][$l] + $matrixArrayB[$k][$l];
-                        print "<td>".$adding."</td>";           // wyswietlanie nowej tabeli z macierzy
-                    }print "<tr>";
-                 
-                }print "</table>";
-             }else{
-                 echo "<div id='error'>Inoperative matrix size for adding</div></br>"; // jeżeli w któryms zapytaniu if wartosc bedzie false wyswietli sie blad
-             }
+                    for ($k=1; $k<=$rowB; $k++){
+                      for ($l=1; $l<=$colB; $l++){
+                        $result[$k][$l] = 0;
+                      $result[$k][$l] += $matrixArrayA[$k][$l] + $matrixArrayB[$k][$l];
+                      }
+                     }
+                     return $result;
         }
 
         public function SubMatrix(){
 
-            $rowA = $this->GetRowNumA();
-            $rowB = $this->GetRowNumB();
-            $colA = $this->GetColNumA();
-            $colB = $this->GetColNumB();
+            $rowA = $this->row1;
+            $rowB = $this->row2;
+            $colA = $this->col1;
+            $colB = $this->col2;
             $matrixArrayA = $this->matrixArrayA;
             $matrixArrayB = $this->matrixArrayB;
-            if ($rowA==$rowB && $colA==$colB){          // Sprawdzenie czy macierze sa tego samego rozmiaru
-                print "<table class='resultTable'>";
-                print "<caption>Subtracting result</caption>";
                     for ($k=1; $k<=$rowB; $k++){
-                    print "<tr>";
                       for ($l=1; $l<=$colB; $l++){
-                      $subtraction = $matrixArrayA[$k][$l] - $matrixArrayB[$k][$l];
-                      print "<td>".$subtraction."</td>"; // wyswietlanie nowej tabeli z macierzy
-                      }print "<tr>";
-                     }print "</table>\n";
-            }else{
-           echo "<div id='error'>Inoperative matrix size for subtracting</div></br>"; // jeżeli w któryms zapytaniu if wartosc bedzie false wyswietli sie blad
-            } 
+                        $result[$k][$l] = 0;
+                        $result[$k][$l] += $matrixArrayA[$k][$l] - $matrixArrayB[$k][$l];
+                      }
+                     }
+                     return $result;
         }
 
         public function MultMatrix(){
 
-            $rowA = $this->GetRowNumA();
-            $rowB = $this->GetRowNumB();
-            $colA = $this->GetColNumA();
-            $colB = $this->GetColNumB();
+            $rowA = $this->row1;
+            $rowB = $this->row2;
+            $colA = $this->col1;
+            $colB = $this->col2;
             $matrixArrayA = $this->matrixArrayA;
-            $matrixArrayB = $this->matrixArrayB;
-            if($colA != $rowB){   // sprawdzenie poprawnosci warunku dla mnozenia tutaj Sprawdzenie czy ilosc kolumn macierzy A jest równa ilosci wierszy macierzy B
-            echo "<div id='error'></br>Inoperative matrix sizes for multiplying</div>";
-            }else{
-                print "<table class='resultTable'>";
-                print "<caption>Multiplying result</caption>";
+            $matrixArrayB = $this->matrixArrayB;   
                     for ($i=1; $i <=$rowA; $i++){
-                        print "<tr>";
                         for($j=1; $j <=$colB; $j++){           
                             $result[$i][$j] = 0;
                             for($k=1; $k <=$rowB; $k++){
-                                $multiply = $result[$i][$j] += $matrixArrayA[$i][$k] * $matrixArrayB[$k][$j]; // do zmiennej $result[$i][$j] zostanie dodany wynik mnożenia odpowiednich pól macierzy
-                            }print "<td>".$multiply."</td>";
-                        }print "<tr>";
-                    } print "</table>";     
-            }
+                                $result[$i][$j] += $matrixArrayA[$i][$k] * $matrixArrayB[$k][$j]; // do zmiennej $result[$i][$j] zostanie dodany wynik mnożenia odpowiednich pól macierzy
+                            }
+                        }
+                    }   
+            return $result;
         }
 
-        public function DetMatrix(){
-
-            $rowA = $this->GetRowNumA();
-            $rowB = $this->GetRowNumB();
-            $colA = $this->GetColNumA();
-            $colB = $this->GetColNumB();
+        public function DetMatrix3A(){
             $matrixArrayA = $this->matrixArrayA;
-            $matrixArrayB = $this->matrixArrayB;
-            if ($rowA == $colA && $rowA == 3 && $rowB == $colB && $rowB == 3){
+        
                 $determinantA = $matrixArrayA[1][1]*$matrixArrayA[2][2]*$matrixArrayA[3][3] +
                             $matrixArrayA[1][2]*$matrixArrayA[2][3]*$matrixArrayA[3][1] +
                             $matrixArrayA[1][3]*$matrixArrayA[2][1]*$matrixArrayA[3][2] -
@@ -124,23 +76,40 @@ class Actions extends Maths{
                             $matrixArrayA[1][2]*$matrixArrayA[2][1]*$matrixArrayA[3][3] -
                             $matrixArrayA[1][3]*$matrixArrayA[2][2]*$matrixArrayA[3][1];
         
-                $determinantB = $matrixArrayB[1][1]*$matrixArrayB[2][2]*$matrixArrayB[3][3] +
-                            $matrixArrayB[1][2]*$matrixArrayB[2][3]*$matrixArrayB[3][1] +
-                            $matrixArrayB[1][3]*$matrixArrayB[2][1]*$matrixArrayB[3][2] -
-                            $matrixArrayB[1][1]*$matrixArrayB[2][3]*$matrixArrayB[3][2] -
-                            $matrixArrayB[1][2]*$matrixArrayB[2][1]*$matrixArrayB[3][3] -
-                            $matrixArrayB[1][3]*$matrixArrayB[2][2]*$matrixArrayA[3][1];         
+                return $determinantA;
+               
+        }   
+
+        public function DetMatrix3B(){
+            $matrixArrayB = $this->matrixArrayB;
+
+            $determinantB = $matrixArrayB[1][1]*$matrixArrayB[2][2]*$matrixArrayB[3][3] +
+            $matrixArrayB[1][2]*$matrixArrayB[2][3]*$matrixArrayB[3][1] +
+            $matrixArrayB[1][3]*$matrixArrayB[2][1]*$matrixArrayB[3][2] -
+            $matrixArrayB[1][1]*$matrixArrayB[2][3]*$matrixArrayB[3][2] -
+            $matrixArrayB[1][2]*$matrixArrayB[2][1]*$matrixArrayB[3][3] -
+            $matrixArrayB[1][3]*$matrixArrayB[2][2]*$matrixArrayB[3][1];      
+
+            return $determinantB;
+
+        }
+
+        public function DetMatrix2A(){
+            $matrixArrayA = $this->matrixArrayA;
         
-                print "<div id='det'></br>Matrix A determinant = <b>".$determinantA."</br></b> Matrix B determinant = <b>".$determinantB."</b>";
-            }elseif ($rowA == $colA && $rowA == 2 && $rowB == $colB && $rowB == 2){
                 $determinantA = $matrixArrayA[1][1]*$matrixArrayA[2][2]-$matrixArrayA[1][2]*$matrixArrayA[2][1];
         
-                $determinantB = $matrixArrayB[1][1]*$matrixArrayB[2][2]-$matrixArrayB[1][2]*$matrixArrayB[2][1];
+                return $determinantA;
+               
+        }   
         
-                print "<div id='det'></br>Matrix A determinant = <b>".$determinantA."</br></b> Matrix B determinant = <b>".$determinantB."</b>";
-            }else{
-                echo "<div id='error'></br>Inoperative matrix sizes for determining the determinant";
+        public function DetMatrix2B(){
+            $matrixArrayB = $this->matrixArrayB;
+        
+                $determinantB = $matrixArrayB[1][1]*$matrixArrayB[2][2]-$matrixArrayB[1][2]*$matrixArrayB[2][1];;
+        
+                return $determinantB;
+                
             }
-        }
     }
 ?>
